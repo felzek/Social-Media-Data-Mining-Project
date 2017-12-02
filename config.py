9144d42a7e582fae9d0a8b569f507f8db480c272
ykpengba <yukunpengba@gmail.com>
2017-12-01 21:46:48 -0600

    Delete config.py

--- config.py
+++
@@ -1,5 +0,0 @@
-# Twitter API Keys
-consumer_key = "l6bFO81gyHtJZN3tmolgQYV8U"
-consumer_secret = "YMF1paj2qGZaTrl3yNFqpnqY8i0SV1wz8U2xPWhI7etrNyw6l1"
-access_token = "4310668813-LKW6PbDYAMJOyo1UrSUVfyocTfMHdHCbxsNYcFa"
-access_token_secret = "cd85rMXpBlLoTqs1ljt1zRrQ3I0ZXSXoks15pvgcLqhgC"
